ef1370e1b4f5841eabb3d6f24858a6d1c35a5cda
Peter Myszkowski <m@DESKTOP-IHP4ICM>
2023-01-16 20:28:52 -0600

    finish all states & endpoints, etc. Refactor

--- src/BoLTracker.hs
+++ src/BoLTracker.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -17,6 +18,7 @@ module BoLTracker
     BoLSignature (..),
     BoLTrackerActions (..),
     StartBTP (..),
+    UnloadFreightParams (..),
     startEndpoint,
     useEndpoints,
     useEndpoints',
@@ -34,9 +36,10 @@ import Ledger.Constraints as Constraints
 import Ledger.Typed.Scripts qualified as Scripts
 import Plutus.Contract as Contract
 import Plutus.Contract.StateMachine
+import Plutus.Script.Utils.V2.Typed.Scripts (tyTxOutData, tyTxOutRefOut)
 import PlutusTx qualified
 import PlutusTx.Prelude hiding (Semigroup (..), check, unless)
-import Prelude (Show (..))
+import Prelude (Show (..), String)
 import Prelude qualified
 
 -- ON CHAIN --
@@ -57,19 +60,21 @@ data BoLTrackerState
   = Loading
   | InTransit [BuiltinByteString]
   | InTransitRejected [BuiltinByteString]
-  | Unloaded
-  | Delivered (Maybe BuiltinByteString)
-  | Rejected (Maybe BuiltinByteString)
+  | Delivered BuiltinByteString
+  | Rejected BuiltinByteString
+  | Finished
   deriving (Show, Prelude.Eq)
 
--- instance Eq BoLTrackerState where
---   {-# INLINEABLE (==) #-}
---   Loading == Loading = True
---   (InTransit loc) == (InTransit loc') =
---     loc == loc'
---   Delivered == Delivered = True
---   Rejected == Rejected = True
---   _ == _ = False
+instance Eq BoLTrackerState where
+  {-# INLINEABLE (==) #-}
+  Loading == Loading = True
+  (InTransit loc) == (InTransit loc') =
+    loc == loc'
+  (InTransitRejected loc) == (InTransitRejected loc') =
+    loc == loc'
+  (Delivered _) == (Delivered _) = True
+  (Rejected _) == (Rejected _) = True
+  _ == _ = False
 
 PlutusTx.unstableMakeIsData ''BoLTrackerState
 PlutusTx.makeLift ''BoLTrackerState
@@ -89,7 +94,8 @@ PlutusTx.makeLift ''BoLSignature
 data BoLTrackerActions
   = FinishLoading
   | CurrentLocation BuiltinByteString
-  | UnloadFreight BoLSignature (Maybe BuiltinByteString)
+  | UnloadFreight BoLSignature BuiltinByteString
+  | Claim
   deriving (Show, Prelude.Eq)
 
 PlutusTx.unstableMakeIsData ''BoLTrackerActions
@@ -102,21 +108,23 @@ transition btp s r = case (stateValue s, stateData s, r) of
     Just (Constraints.mustBeSignedBy (btpShipper btp), State (InTransit []) v)
   (v, InTransit locations, CurrentLocation location) ->
     Just (Constraints.mustBeSignedBy (btpCarrier btp), State (InTransit $ locations ++ [location]) v)
-  (v, InTransit _, UnloadFreight Accept maybeNotes) ->
-    Just (Constraints.mustBeSignedBy (btpReciver btp), State (Delivered maybeNotes) v)
-  (v, InTransit _, UnloadFreight Reject maybeNotes) ->
-    Just (Constraints.mustBeSignedBy (btpReciver btp), State (Rejected maybeNotes) v)
+  (v, InTransit _, UnloadFreight Accept notes) ->
+    Just (Constraints.mustBeSignedBy (btpReciver btp), State (Delivered notes) v)
+  (v, InTransit _, UnloadFreight Reject notes) ->
+    Just (Constraints.mustBeSignedBy (btpReciver btp), State (Rejected notes) v)
   (v, Rejected _, CurrentLocation location) ->
     Just (Constraints.mustBeSignedBy (btpCarrier btp), State (InTransitRejected [location]) v)
   (v, InTransitRejected locations, CurrentLocation location) ->
     Just (Constraints.mustBeSignedBy (btpCarrier btp), State (InTransitRejected $ locations ++ [location]) v)
-  (v, InTransitRejected _, UnloadFreight Accept maybeNotes) ->
-    Just (Constraints.mustBeSignedBy (btpCarrier btp), State (Delivered maybeNotes) v)
+  (v, InTransitRejected _, UnloadFreight Accept notes) ->
+    Just (Constraints.mustBeSignedBy (btpShipper btp), State (Delivered notes) v)
+  (_, Delivered _, Claim) ->
+    Just (Constraints.mustBeSignedBy (btpCarrier btp), State Finished mempty)
   _ -> Nothing
 
 {-# INLINEABLE isFinal #-}
 isFinal :: BoLTrackerState -> Bool
-isFinal (Delivered _) = True
+isFinal Finished = True
 isFinal _ = False
 
 {-# INLINEABLE btpStateMachine #-}
@@ -175,7 +183,37 @@ finishLoading :: BoLTrackerParams -> Contract w s Text ()
 finishLoading btp = void $ mapErrorSM $ runStep (btpClient btp) FinishLoading
 
 updateLocation :: BoLTrackerParams -> BuiltinByteString -> Contract w s Text ()
-updateLocation btp location = void $ mapErrorSM $ runStep (btpClient btp) $ CurrentLocation location
+updateLocation btp location = do
+  let client = btpClient btp
+  m <- mapErrorSM $ getOnChainState client
+  case m of
+    Nothing -> throwError "Contract not found"
+    Just (o, _) -> case tyTxOutData $ tyTxOutRefOut $ ocsTxOutRef o of
+      InTransit _ -> do
+        logInfo @String "Freight is in transit. Updating current location..."
+        void $ mapErrorSM $ runStep client $ CurrentLocation location
+      Rejected _ -> do
+        logInfo @String "Freight is rejected. Sending back to shipper..."
+        void $ mapErrorSM $ runStep client $ CurrentLocation location
+      InTransitRejected _ -> do
+        logInfo @String "Freight is being returned. Updating current location..."
+        void $ mapErrorSM $ runStep client $ CurrentLocation location
+      _ -> logInfo @String "Wrong contract state"
+
+unloadFreight :: BoLTrackerParams -> BoLSignature -> BuiltinByteString -> Contract w s Text ()
+unloadFreight btp s notes = void $ mapErrorSM $ runStep (btpClient btp) $ UnloadFreight s notes
+
+claimReward :: BoLTrackerParams -> Contract w s Text ()
+claimReward btp = void $ mapErrorSM $ runStep (btpClient btp) Claim
+
+data UnloadFreightParams = UnloadFreightParams
+  { bolSignature :: BoLSignature,
+    notes :: BuiltinByteString
+  }
+  deriving (Show, Generic, FromJSON, ToJSON)
+
+PlutusTx.unstableMakeIsData ''UnloadFreightParams
+PlutusTx.makeLift ''UnloadFreightParams
 
 type BTPStartSchema =
   Endpoint "start contract" StartBTP
@@ -183,6 +221,8 @@ type BTPStartSchema =
 type BTPUseSchema =
   Endpoint "finish loading" ()
     .\/ Endpoint "update location" BuiltinByteString
+    .\/ Endpoint "unload freight" UnloadFreightParams
+    .\/ Endpoint "claim reward" ()
 
 startEndpoint :: Contract (Last BoLTrackerParams) BTPStartSchema Text ()
 startEndpoint =
@@ -194,14 +234,18 @@ startEndpoint =
 
 useEndpoints' ::
   ( HasEndpoint "finish loading" () s,
-    HasEndpoint "update location" BuiltinByteString s
+    HasEndpoint "update location" BuiltinByteString s,
+    HasEndpoint "unload freight" UnloadFreightParams s,
+    HasEndpoint "claim reward" () s
   ) =>
   BoLTrackerParams ->
   Promise () s Text ()
-useEndpoints' btp = finishLoading' `select` updateLocation'
+useEndpoints' btp = finishLoading' `select` updateLocation' `select` unloadFreight' `select` claimReward'
   where
     finishLoading' = endpoint @"finish loading" $ \() -> handleError logError (finishLoading btp)
     updateLocation' = endpoint @"update location" $ \location -> handleError logError (updateLocation btp location)
+    unloadFreight' = endpoint @"unload freight" $ \UnloadFreightParams {bolSignature, notes} -> handleError logError (unloadFreight btp bolSignature notes)
+    claimReward' = endpoint @"claim reward" $ \() -> handleError logError (claimReward btp)
 
 useEndpoints :: BoLTrackerParams -> Contract () BTPUseSchema Text ()
 useEndpoints = forever . awaitPromise . useEndpoints'
--- src/TestBoLTracker.hs
+++ src/TestBoLTracker.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE TypeApplications #-}
 
 module TestBoLTracker
   ( runMyTrace,
+    TraceType (..),
   )
 where
 
@@ -12,9 +15,11 @@ import BoLTracker
 import BoLTracker qualified as BT
 import Control.Monad hiding (fmap)
 import Control.Monad.Freer.Extras as Extras
+import Data.Aeson (FromJSON, ToJSON)
 import Data.Default (Default (..))
 import Data.Map qualified as Map
 import Data.Monoid (Last (..))
+import GHC.Generics (Generic)
 import Ledger
 import Ledger.Ada as Ada
 import Plutus.Contract.Test
@@ -24,8 +29,12 @@ import PlutusTx.Prelude
 import Wallet.Emulator qualified as EM
 import Prelude (IO, Show (..), String)
 
-runMyTrace :: IO ()
-runMyTrace = runEmulatorTraceIO' def emCfg myTrace
+data TraceType = AllGood | Rejected
+  deriving (Show, Generic, FromJSON, ToJSON)
+
+runContract :: TraceType -> IO ()
+runContract AllGood = runEmulatorTraceIO' def emCfg shipFreightAllGood
+runContract Rejected = runEmulatorTraceIO' def emCfg shipFreightRejectFullLoad
 
 emCfg :: EmulatorConfig
 emCfg = EmulatorConfig (Left $ Map.fromList [(knownWallet w, v) | w <- [1 .. 3]]) def
@@ -33,8 +42,8 @@ emCfg = EmulatorConfig (Left $ Map.fromList [(knownWallet w, v) | w <- [1 .. 3]]
     v :: Value
     v = Ada.lovelaceValueOf 1000_000_000
 
-myTrace :: EmulatorTrace ()
-myTrace = do
+shipFreightAllGood :: EmulatorTrace ()
+shipFreightAllGood = do
   h <- activateContractWallet w1 BT.startEndpoint
 
   let sbtp =
@@ -42,7 +51,7 @@ myTrace = do
           { sbtpShipper = EM.mockWalletPaymentPubKeyHash w1,
             sbtpCarrier = EM.mockWalletPaymentPubKeyHash w2,
             sbtpReciver = EM.mockWalletPaymentPubKeyHash w3,
-            sbtpBoLURL = stringToBuiltinByteString "a"
+            sbtpBoLURL = stringToBuiltinByteString "url to version contol"
           }
 
   callEndpoint @"start contract" h sbtp
@@ -55,7 +64,7 @@ myTrace = do
 
       h1 <- activateContractWallet w1 $ useEndpoints btp
       h2 <- activateContractWallet w2 $ useEndpoints btp
-      -- h3 <- activateContractWallet w3 $ useEndpoints btp
+      h3 <- activateContractWallet w3 $ useEndpoints btp
 
       callEndpoint @"finish loading" h1 ()
       void $ Emulator.waitNSlots 5
@@ -67,4 +76,76 @@ myTrace = do
       void $ Emulator.waitNSlots 5
 
       callEndpoint @"update location" h2 $ stringToBuiltinByteString "Portland, OR"
+      void $ Emulator.waitNSlots 5
+
+      let ufp =
+            UnloadFreightParams
+              { bolSignature = Accept,
+                notes = stringToBuiltinByteString "All Good"
+              }
+      callEndpoint @"unload freight" h3 ufp
+      void $ Emulator.waitNSlots 5
+
+      callEndpoint @"claim reward" h2 ()
+      void $ Emulator.waitNSlots 5
+
+shipFreightRejectFullLoad :: EmulatorTrace ()
+shipFreightRejectFullLoad = do
+  h <- activateContractWallet w1 BT.startEndpoint
+
+  let sbtp =
+        StartBTP
+          { sbtpShipper = EM.mockWalletPaymentPubKeyHash w1,
+            sbtpCarrier = EM.mockWalletPaymentPubKeyHash w2,
+            sbtpReciver = EM.mockWalletPaymentPubKeyHash w3,
+            sbtpBoLURL = stringToBuiltinByteString "url to version contol"
+          }
+
+  callEndpoint @"start contract" h sbtp
+  void $ Emulator.waitNSlots 5
+  Last m <- observableState h
+  case m of
+    Nothing -> Extras.logError @String "error starting token sale"
+    Just btp -> do
+      Extras.logInfo $ "started loading freight" ++ show btp
+
+      h1 <- activateContractWallet w1 $ useEndpoints btp
+      h2 <- activateContractWallet w2 $ useEndpoints btp
+      h3 <- activateContractWallet w3 $ useEndpoints btp
+
+      callEndpoint @"finish loading" h1 ()
+      void $ Emulator.waitNSlots 5
+
+      callEndpoint @"update location" h2 $ stringToBuiltinByteString "Chicago, IL"
+      void $ Emulator.waitNSlots 5
+
+      callEndpoint @"update location" h2 $ stringToBuiltinByteString "Denver, CO"
+      void $ Emulator.waitNSlots 5
+
+      callEndpoint @"update location" h2 $ stringToBuiltinByteString "Portland, OR"
+      void $ Emulator.waitNSlots 5
+
+      let ufp1 =
+            UnloadFreightParams
+              { bolSignature = Reject,
+                notes = stringToBuiltinByteString "Broken Seal"
+              }
+      callEndpoint @"unload freight" h3 ufp1
+      void $ Emulator.waitNSlots 5
+
+      callEndpoint @"update location" h2 $ stringToBuiltinByteString "Denver, CO"
+      void $ Emulator.waitNSlots 5
+
+      callEndpoint @"update location" h2 $ stringToBuiltinByteString "Chicago, IL"
+      void $ Emulator.waitNSlots 5
+
+      let ufp2 =
+            UnloadFreightParams
+              { bolSignature = Accept,
+                notes = stringToBuiltinByteString "Recived returned freight"
+              }
+      callEndpoint @"unload freight" h1 ufp2
+      void $ Emulator.waitNSlots 5
+
+      callEndpoint @"claim reward" h2 ()
       void $ Emulator.waitNSlots 5
